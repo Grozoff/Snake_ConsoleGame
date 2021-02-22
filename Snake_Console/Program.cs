793b8af0617d414b024b8a868d23a5b08c653149
Sergey Grozov <grozoff92@gmail.com>
2021-02-22 19:00:23 +0500

    add game over upd draw walls

--- Snake_Console/Program.cs
+++ Snake_Console/Program.cs
@@ -14,20 +14,15 @@ namespace Snake_Console
 #pragma warning restore CA1416 // Validate platform compatibility
 
             // Рамка
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();            
 
             // Змейка
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            // Еда
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
             food.Draw();
@@ -35,6 +30,10 @@ namespace Snake_Console
 
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail() )
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
